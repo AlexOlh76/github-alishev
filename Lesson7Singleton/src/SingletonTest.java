2edc7d41f51d35f9e836cc78e274ab90a07df8f2
Alex <in76gener@gmail.com>
2024-04-23 00:29:18 +0300

    fierst commit

---
+++ Lesson7Singleton/src/SingletonTest.java
@@ -0,0 +1,17 @@
+//Тестируем
+
+public class SingletonTest {
+    public static void main(String[] args) {
+        Lesson7Singleton first = Lesson7Singleton.getInstance("Hello!");
+        Lesson7Singleton second = Lesson7Singleton.getInstance("Goodbye!");
+
+        // true
+        System.out.println(first == second);
+
+        // Hello!
+        System.out.println(first.getValue());
+
+        // Hello!
+        System.out.println(second.getValue());
+    }
+}
